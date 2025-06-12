148fe48c1938b1f2c0c5a0b16b88707348761e95
にとう ひさし <97216430+NITOH-Hisashi@users.noreply.github.com>
2025-06-12 18:14:31 +0900

    config.jsの修正とサンプルイベントの追加
    config.jsでは、CONFIGオブジェクトのエクスポートとコメントアウトされていた行が削除されました。
    main.jsでは、createApp関数に3つのサンプルイベントが追加され、loadEvents関数ではaccessTokenがない場合にサンプルイベントを表示する処理が追加されました。

--- config.js
+++ config.js
@@ -2,5 +2,3 @@ const CONFIG = {
     GOOGLE_CLIENT_ID: "154186872790-h7pgkhai8fb7cs0i076n5n05dvqp9b6e.apps.googleusercontent.com"
 };
 
-export default CONFIG;
-//                 callback: handleCredentialResponse,
