5a867ab0879df761da67f173de91ea7e60063024
NITOH Hisashi <support@amus.biz>
2025-08-08 03:36:08 +0900

    base64処理を元に戻してみる

--- main.js
+++ main.js
@@ -502,9 +502,15 @@ createApp({
             const base64Url = token.split('.')[1];
             console.log({ base64Url });
             // Use the new decode function
+            /*
             const base64 = base64UrlDecode(base64Url);
             console.log({ base64 });
             const jsonPayload = decodeURIComponent(base64.split('').map(function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+            */
+            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
                 return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
             }).join(''));
 
